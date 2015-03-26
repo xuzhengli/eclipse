c63f4e63dc0f3b393d403f8fe89b5f40a981a1e8
徐正礼 <xuzhl@xuzhenglideMacBook-Pro.local>
2015-03-26 18:39:32 +0800

    解决中文到问题

--- src/org/Test.java
+++ src/org/Test.java
@@ -4,8 +4,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("---输出一点有用的信息好吧????");
-        //添加上一点注释进行测试
+        System.out.println("--????");
+        //th
 	}
 
 }
